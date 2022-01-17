32c64e91093b033ef37db27ea5413c4a7ecf0c32
shidomnt <hatranquang02@gmail.com>
2022-01-17 14:31:59 +0700

    ignore ruby

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,26 +1,7 @@
 require("nvim-treesitter.configs").setup({
-	ensure_installed = {
-		"c",
-		"cpp",
-		"css",
-		"c_sharp",
-		"hjson",
-		"html",
-		"javascript",
-		"json",
-		"json5",
-		"jsonc",
-		"lua",
-		"php",
-		"python",
-		"regex",
-		"scss",
-		"tsx",
-		"typescript",
-		"vim",
-	}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-	ignore_install = {}, -- List of parsers to ignore installing
+	ignore_install = {"ruby"}, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		disable = {}, -- list of language that will be disabled
