7bf86837cfbd05461b2c68029e94768c8c0c6b61
shidomnt <hatranquang02@gmail.com>
2022-02-17 13:13:29 +0700

    disable netrw

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -3,12 +3,16 @@ vim.g.nvim_tree_git_hl = 1
 vim.highlight.NvimTreeFolderIcon = "guibg=blue"
 vim.g.nvim_tree_respect_buf_cwd = 1
 require("nvim-tree").setup({
+  disable_netrw = true,
 	update_cwd = true,
 	update_focused_file = {
 		enable = true,
 		update_cwd = true,
 		ignore_list = {},
 	},
+  diagnostics = {
+    enable = true,
+  },
 	view = {
 		auto_resize = true,
 	},
